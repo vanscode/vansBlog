d8cc29e63e2741ecd32556121c67d4f674f6e523
vanscode <vanscode@qq.com>
2020-10-26 12:24:46 +0800

    init blog

---
+++ docs/.vuepress/components/button.vue
@@ -0,0 +1,12 @@
+<template>
+    <button @click="handleClick">我是一个按钮</button>
+</template>
+<script>
+    export default {
+        method: {
+            handleClick() {
+                console.log("我是一个按钮")
+            }
+        }
+    }
+</script>
